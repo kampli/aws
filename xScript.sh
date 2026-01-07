5512c1a8bb9a733653e01ef069625cc5884f5328
Shivakumar Kampli <kampli@150.sub-97-11-234.myvzw.com>
2026-01-07 14:14:57 +0000

    Updating to repo xScript.sh

--- xScript.sh
+++ xScript.sh
@@ -48,6 +48,28 @@ function deleteStack() {
         --stack-name lamp-stack
 }
 
+function getOutputs() {
+    echo "AWS_PROFILE == ${AWS_PROFILE}"
+    aws cloudformation describe-stacks \
+        --stack-name lamp-stack \
+        --query "Stacks[0].Outputs"
+}
+
+function loginToEC2() {
+    echo "getting IP Address for the EC2 instance"
+    aws cloudformation describe-stacks \
+        --stack-name lamp-stack \
+        --query "Stacks[0].Outputs[?OutputKey=='InstancePublicIP'].OutputValue" \
+        --output text
+    
+    ipAddress_EC2=$(aws cloudformation describe-stacks \
+                        --stack-name lamp-stack \
+                        --query "Stacks[0].Outputs[?OutputKey=='InstancePublicIP'].OutputValue" \
+                        --output text);
+    echo "ipAddress_EC2 = ${ipAddress_EC2}"
+    ssh -o StrictHostKeyChecking=accept-new -i ./testkeypair.pem ec2-user@${ipAddress_EC2}
+}
+
 if [ $# -gt 0 ] ; then
     ${@}
 else
