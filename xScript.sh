dd04e9fefd2fc2e87086e53b215d5c9cf1b6c9b4
Shivakumar Kampli <kampli@150.sub-97-11-234.myvzw.com>
2026-01-07 11:02:31 +0000

    xScript

---
+++ xScript.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function listFunctions() {
+
+    grep function $0 | egrep -v "listFunctions|^ *#" | sed 's/ *function //g' | sed 's/(.*//g' | sed 's/ .*//g' | sort
+
+}
+
+function addFilesToRepo() {
+
+    git add ${@}
+    git commit -m "adding to repo ${@}"
+
+    git push
+}
+
+
+function updateFilesToRepo() {
+
+    git add ${@}
+    git commit -m "adding to repo ${@}"
+
+    git push
+}
+
+if [ $# -gt 0 ] ; then
+    ${@}
+else
+    echo "Invalid call"
+    listFunctions
+
+fi
+
